2bfdeb1b7da1481e62a81079e9582ef8f0eae75b
AlejandroLargo <alejandropulidolargo@gmail.com>
2024-09-25 10:18:52 -0500

    cambio de tipo de variable producto

--- src/main/java/Proyecto_final/Producto.java
+++ src/main/java/Proyecto_final/Producto.java
@@ -9,19 +9,19 @@ public class Producto {
             case 1:
                 System.out.println("----Computadores----");
                 String[] computadores = {"Laptop", "PC Gamer", "Macbook"};
-                int[] preciosComputadores = {2500000, 4000000, 3000000};
+                double[] preciosComputadores = {25000000, 4000000, 3000000};
                 mostrarProductos(computadores, preciosComputadores);
                 break;
             case 2:
                 System.out.println("----Celulares----");
                 String[] celulares = {"iPhone", "Samsung Galaxy", "Huawei"};
-                int[] preciosCelulares = {6000000, 5000000, 6500000};
+                double[] preciosCelulares = {6000000, 5000000, 6500000};
                 mostrarProductos(celulares, preciosCelulares);
                 break;
             case 3:
                 System.out.println("----Consolas----");
                 String[] consolas = {"PlayStation", "Xbox", "Nintendo Switch"};
-                int[] preciosConsolas = {3000000, 4500000, 2500000};
+                double[] preciosConsolas = {3000000, 4500000, 2500000};
                 mostrarProductos(consolas, preciosConsolas);
                 break;
             default:
@@ -30,7 +30,7 @@ public class Producto {
         }
     }
 
-    private void mostrarProductos(String[] productos, int[] precios) {
+    private void mostrarProductos(String[] productos, double[] precios) {
         for (int i = 0; i < productos.length; i++) {
             System.out.println((i + 1) + ". " + productos[i] + " - $" + precios[i]);
         }
